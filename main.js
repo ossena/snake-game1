a7f65078d514211bb1fb672b5004e1d8096e54d8
ossena <alessandro.ossena@myenaip.eu>
2024-12-11 08:56:43 +0000

    Aggiornamento 2

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-alert("ciao 2C");
+let posSnakeX =0;
+let posSnakeY =0;
+
+let velocitaX = 1;
+let velocitaY = 1;
+
+let direzioneX = 1;
+let direzioneY = 0;
+
+let posMelaX = 20;
+let posMelaY = 20;
+
+let latoSnake = 1;
+
+
+function setup(){
+    createCanvas(400,400);
+    frameRate(5);
+    scale(10);
+}
+
+function draw(){
+    background(200,200,255);
+    /*qui definiamo lo snake*/
+    scale(10);
+    posSnakeX = posSnakeX + velocitaX * direzioneX;
+    posSnakeY = posSnakeY + velocitaY * direzioneY;
+    square(posSnakeX,posSnakeY,latoSnake)
+}
+
+function keyPressed(){
+    switch (keyCode) {
+        case UP_ARROW:
+            direzioneX = 0;
+            direzioneY = -1;
+            break;
+        case DOWN_ARROW:
+            direzioneX = 0;
+            direzioneY = 1;
+            break;
+        case RIGHT_ARROW:
+            direzioneX = 1;
+            direzioneY = 0;
+            break;
+        case LEFT_ARROW:
+            direzioneX = -1;
+            direzioneY = 0;
+            break;
+                
+
+
+        default:
+            break;
+    }
+
+
+}
